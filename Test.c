9b0b1966529ec788a687add8b6bac4d245c30d90
Stephan KWAN <stephan.kwan@univ-tlse3.fr>
2018-12-04 22:59:38 +0000

    modif

--- Test.c
+++ Test.c
@@ -3,15 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-typedef struct Coordo Coordo
-struct Coordo
-{
-	float x;
-	float y;
-	float z;
-};
-
-typedef struct Parametre Parametre
+typedef struct Parametre Parametre;
 struct Parametre
 {
 	float sigma;
@@ -21,27 +13,34 @@ struct Parametre
 	float dt;
 };
 
-void Creation_Parametre(Parametre *par)
+void Creation_Parametre(Parametre* par)
 {
 	
 	printf("Entrez les parametres sigma, rho, beta\n");
 	
 	printf("Sigma : ");
-	scanf ("%f",&par.sigma);
+	scanf ("%f",&(par.sigma));
 	printf("\n");
 	printf("Rho : ");
-	scanf ("%f",&par.rho);
+	scanf ("%f",&(par.rho));
 	printf("\n");
 	printf("Beta : ");
-	scanf ("%f",&par;beta);
+	scanf ("%f",&(par.beta));
 	printf("\n");
 	
 	printf("Entrez le Tmax et l'incrementation dt");
 	
 	printf("Tmax : ");
-	scanf ("%f",&par.Tmax);
+	scanf ("%f",&(par.Tmax));
 	printf("\n");
 	printf("dt : ");
-	scanf ("%f",&par.dt);
+	scanf ("%f",&(par.dt));
 }
 
+int main ( int argc , char * argv [ ] )
+{
+    Parametre par ;
+    Creation_Parametre(&par);
+    
+    printf("%f,%f,%f,%f,%f",par.sigma, par.rho, par.beta, par.Tmax, par.dt);
+}
