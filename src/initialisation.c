90a89c6fed0f0d7771aeccdc9121299200975b19
Luguy GONTHIER <luguy.gonthier@univ-tlse3.fr>
2018-12-05 20:36:37 +0000

    Ajout fichier et calcul

--- src/initialisation.c
+++ src/initialisation.c
@@ -2,17 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define DT_MAX 0.1
+#define DT_MIN 0.0001
+#define TMAX 1000
+#define TMIN 10
 
-typedef struct Coordo Coordo
-struct Coordo
+struct Coordo_s
 {
 	float x;
 	float y;
 	float z;
 };
+typedef struct Coordo_s Coordo;
 
-typedef struct Parametre Parametre
-struct Parametre
+struct Parametre_s
 {
 	float sigma;
 	float rho;
@@ -20,28 +23,51 @@ struct Parametre
 	float Tmax;
 	float dt;
 };
+typedef struct Parametre_s Parametre;
 
-void Creation_Parametre(Parametre *par)
+void Creation_Parametre(Parametre* par)
 {
 	
 	printf("Entrez les parametres sigma, rho, beta\n");
 	
 	printf("Sigma : ");
-	scanf ("%f",&par.sigma);
+	scanf ("%f",&par->sigma);
 	printf("\n");
 	printf("Rho : ");
-	scanf ("%f",&par.rho);
+	scanf ("%f",&par->rho);
 	printf("\n");
 	printf("Beta : ");
-	scanf ("%f",&par;beta);
+	scanf ("%f",&par->beta);
 	printf("\n");
 	
-	printf("Entrez le Tmax et l'incrementation dt");
+	printf("Entrez le Tmax et l'incrementation dt\n");
 	
 	printf("Tmax : ");
-	scanf ("%f",&par.Tmax);
+	scanf ("%f",&par->Tmax);
+	if ((*par).Tmax < TMIN)
+	{
+        (*par).Tmax = TMIN;
+    }
+    else
+    {
+        if ((*par).Tmax > TMAX)
+        {
+            (*par).Tmax = TMAX;
+        }
+    }
 	printf("\n");
 	printf("dt : ");
-	scanf ("%f",&par.dt);
+	scanf ("%f",&par->dt);
+		if ((*par).dt < DT_MIN)
+	{
+        (*par).dt = DT_MIN;
+    }
+    else
+    {
+        if ((*par).dt > DT_MAX)
+        {
+            (*par).dt = DT_MAX;
+        }
+    }
+    printf("\n");
 }
-
